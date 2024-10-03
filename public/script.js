2da6875613c40a9c45b600153c92425350223881
ghostX-64 <sharmaranjeet113@gmail.com>
2024-10-03 10:54:01 +0530

    Added .env file and add to the .gitignore

--- public/script.js
+++ public/script.js
@@ -1,3 +1,23 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.error('MongoDB connection error:', error));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 let bookCount = 0;
 
 function validateInput() {
